5f6330c4c9e40f38601799f277953a1583b5422c
hassaan5621 <mohammadhassaan567@gmail.com>
2024-12-01 12:28:12 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "iqxjQDKB#RSsnRiVZZKG_6uQAv21BnfGNIhfSRmoyTwAVP01oB-A",
   // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
   // add your prifix for bot
@@ -15,15 +15,15 @@ ANTI_BAD: process.env.ANTI_BAD || "false",
 // false kr true Anti-Bad words 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/hzagwo.jpg",
 // add img for alive msg
-ALIVE_MSG: process.env.ALIVE_MSG || "> Zinda Hun Yar *KHAN-AI*⚡",
+ALIVE_MSG: process.env.ALIVE_MSG || "*_ALWAYS TRUST ALLAH⚡_*",
 // add alive msg here 
-SUDO_NB: process.env.SUDO_NB || "",
+SUDO_NB: process.env.SUDO_NB || "923556419245",
 // add your sudo number like 92314xxxxxx
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
   // add your mongodb
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   // make true or false status auto seen
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
   // make bot public or private 
 AUTO_VOICE: process.env.AUTO_VOICE || "true"
   // make auto voice false or true
